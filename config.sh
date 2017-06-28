be2c4fb616ba00cf145ba1df48e473319a04df37
mmaudet <mmaudet@linagora.com>
2017-06-28 18:01:53 +0200

    Added default colors in config file.

--- config.sh
+++ config.sh
@@ -4,6 +4,12 @@
 HUEIP="192.168.1.11" #Bridge ip --> si vous la connaissez pas / if you don't know : http://www.advanced-ip-scanner.com/fr/
 mapWordLigth="{'ca va':'salon','salope':'salon','salaud':'salon'}"
 mapColor="{
-	'rose': {'hue':322,'sat':200,'bri':100},
-	'orange': {'hue':49,'sat':200,'bri':240}
+	'orange': { 'hue': 8000, 'sat': 254, 'bri': 254 },
+	'jaune': { 'hue': 14000, 'sat': 254, 'bri': 254 },
+	'verre': { 'hue': 25000, 'sat': 254, 'bri': 254 },
+	'vert': { 'hue': 25000, 'sat': 254, 'bri': 254 },
+	'bleu': { 'hue': 42000, 'sat': 254, 'bri': 254 },
+	'violet': { 'hue': 50000, 'sat': 254, 'bri': 254 },
+	'rose': { 'hue': 60000, 'sat': 254, 'bri': 254 },
+	'rouge': { 'hue': 65000, 'sat': 254, 'bri': 254 }
 }"
